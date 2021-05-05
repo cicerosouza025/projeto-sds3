6773115985a5ecc36ad076a295a9f2b2adea37bc
Cicero de Souza <cicero025@live.com>
2021-05-04 22:54:45 -0300

    Basic static component

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/c%C3%ADcero-souza-fran%C3%A7a/" target="_blank" rel="noreferrer">Cícero França</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;

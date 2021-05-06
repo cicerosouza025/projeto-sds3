68d27b21d2f35b981c48b97cdda932468949950f
Cicero de Souza <cicero025@live.com>
2021-05-06 20:00:18 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/cicero/dsvendas/entities/Seller.java
@@ -0,0 +1,38 @@
+package com.cicero.dsvendas.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "tb_sellers")
+@Getter
+@Setter
+public class Seller {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	@OneToMany(mappedBy = "seller")
+	private List<Sale> sales = new ArrayList<>();
+	
+	public Seller() {}
+
+	public Seller(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+}
